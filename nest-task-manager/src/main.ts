72835fdc881dd8557085cdc0277d8215120c3171
jannette-k <onegkuj@gmail.com>
2026-03-03 23:19:53 +0300

    Added .env file to react-task-manager folder

--- nest-task-manager/src/main.ts
+++ nest-task-manager/src/main.ts
@@ -5,8 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    origin: ['http://localhost:8080'],  //frontend url
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
